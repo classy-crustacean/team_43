97f579b0ea97e7dc2fc7bad860be5eba38b0cb67
Joe <jrosloni@purdue.edu>
2022-03-04 18:06:56 +0000

    random numbers working in odometry Demo

--- odometryDemo.py
+++ odometryDemo.py
@@ -1,13 +1,13 @@
-import numpy as np
 import time
 import socket
+import random
 
-UDP_INFO = ("10.1.1.10", 5005)
+UDP_INFO = ("10.1.1.102", 5005)
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 
 for i in range(10):
-    y = np.random.random()
+    y = random.randrange(10)
     message = "%d,%d" % (i, y)
     sock.sendto(message.encode('utf-8'), UDP_INFO)
     time.sleep(0.05)
