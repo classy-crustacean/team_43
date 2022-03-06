190a4dde035e9fc992cc4ee5f167c34ff5f4b311
Joe Rosloniec <joerosloniec@gmail.com>
2022-03-06 10:28:00 -0500

    Odometry demo ready to test w/o movement

--- odometryDemo.py
+++ odometryDemo.py
@@ -1,15 +1,57 @@
+from math import cos, sin, pi
 import time
 import socket
-import random
+import os
+import re
+from brickpi3 import BrickPi3
 
-UDP_INFO = ("10.1.1.102", 5005)
+BP = BrickPi3()
+rightMotor = BP.PORT_A
+leftMotor = BP.PORT_D
+
+wheelDiameter = 5
+width = 15
+
+stream = os.popen("who am i")
+output = stream.read()
+
+UDP_INFO = (re.search("\d+\.\d+\.\d+\.\d+"), 5005)
+print(UDP_INFO)
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+# set robot start position to origin, pointing straight up
+x = 0
+y = 0
+robotAngle = pi / 2
+message = "%f,%f" % (x, y)
+sock.sendto(message.encode('utf-8'), UDP_INFO)
 
-for i in range(10):
-    y = random.randrange(10)
-    message = "%d,%d" % (i, y)
-    sock.sendto(message.encode('utf-8'), UDP_INFO)
-    time.sleep(0.05)
+try:
+    BP.offset_motor_encoder(rightMotor, BP.get_motor_encoder(rightMotor))
+    BP.offset_motor_encoder(leftMotor, BP.get_motor_encoder(leftMotor))
 
-sock.sendto("stop".encode('utf-8'), UDP_INFO)
+    while True:
+        leftLength = BP.get_motor_encoder(leftMotor)
+        rightLength = BP.get_motor_encoder(rightMotor)
+
+        if (leftLength == rightLength):
+            x += leftLength*cos(robotAngle)
+            y += leftLength*sin(robotAngle)
+        else:
+            radius = (width * (leftLength + rightLength)) / (2 * (leftLength - rightLength))
+            arcAngle = (leftLength - rightLength) / width
+            x += radius(sin(robotAngle) - cos(arcAngle) * sin(robotAngle) \
+            + sin(arcAngle) * cos(robotAngle))
+            y += radius(-cos(robotAngle) + cos(arcAngle) * cos(robotAngle) \
+            + sin(arcAngle) * sin(robotAngle))
+            
+            
+        message = "%f,%f" % (x, y)
+        sock.sendto(message.encode('utf-8'), UDP_INFO)
+        time.sleep(0.05)
+
+    BP.reset_all()
+    sock.sendto("stop".encode('utf-8'), UDP_INFO)
+except KeyboardInterrupt:
+    BP.reset_all()
+    sock.sendto("stop".encode('utf-8'), UDP_INFO)
