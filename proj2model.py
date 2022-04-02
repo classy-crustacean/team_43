4bdac5ae9438612f9db228cd65bd26944aaea47e
James <100536256+James-Talavage@users.noreply.github.com>
2022-04-02 10:49:04 -0400

    Update proj2model.py

--- proj2model.py
+++ proj2model.py
@@ -22,18 +22,20 @@ P_a = 0.9093
 d = ?
 a = ((1/4)*(math.pi)*(d_p)**2)
 sigma = ?
-p = p_s-p_a
+p = p_s-p_a # The density of the smog minus the air
 
 import math
 # Drag is in the x-direction. Everything else is in the y-direction.
 a = (-(m_p * g) - ((1/2)(c_d * (p_s - p_a) * a * u_r * u__r)) + ((1/6) * (math.pi) * (p_s - p_a) * g * (d**3)) + ((sigma * voltage) / (area_pp * dist_pp))) / (m_p)
-a_x = -1/2*c_D * p *a *u_r *u_r
-a_y = (-m_p*g + 1/6 * math.pi * p *g * d**3 + sigma * voltage / (area_pp * dist_pp))/m_p
-t = (u_r - math.sqrt(u_r**2-4*(1/4*c_D * p *a *u_r *u_r)(length_pp)))/(1/2**c_D * p *a *u_r *u_r)
+a_x = -1/2*c_D * p *a *u_r *u_r # The acceleration in the x-direction
+a_y = (-m_p*g + 1/6 * math.pi * p *g * d**3 + sigma * voltage / (area_pp * dist_pp))/m_p # The acceleration in the y-direction
+t = (u_r - math.sqrt(u_r**2-4*(1/4*c_D * p *a *u_r *u_r)(length_pp)))/(1/2**c_D * p *a *u_r *u_r) # The time it takes one particle to travel from one side of the ESP to the other
 if t < 0:
-  t = (u_r + math.sqrt(u_r**2-4*(1/4*c_D * p *a *u_r *u_r)(length_pp)))/(1/2**c_D * p *a *u_r *u_r)
+  t = (u_r + math.sqrt(u_r**2-4*(1/4*c_D * p *a *u_r *u_r)(length_pp)))/(1/2**c_D * p *a *u_r *u_r) # # The time it takes one particle to travel from one side of the ESP to the other if
+  # the initial calculation for t was negative
 
-greatest_distance = 1/2(a_y)*t**2
+greatest_distance = 1/2(a_y)*t**2 # The furthest below the top plate that the particles can be before they are no longer picked up by the electrostaic participator
+# Efficiency calculations
 if greatest distance > dist_pp:
   efficiency = 100
 else:
