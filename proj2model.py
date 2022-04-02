c883bba496d9166677d20f740bbeadad5cf50e1c
James <100536256+James-Talavage@users.noreply.github.com>
2022-04-02 11:49:49 -0400

    Update proj2model.py

--- proj2model.py
+++ proj2model.py
@@ -16,22 +16,21 @@ particle_size = 0
 while (particle_size == 0)
   particle_size = int(input("Choose particle size:\n1: PM2.5\n2: PM10\n"))
   if (particle_size == 1):
-    m_p = ?
     d = 0.0000000025
     p = 0.0000000092
   elif (particle_size == 2):
-    m_p = ?
     d = 0.00000001
     p = 0.0000000116
   else:
     print("invalid particle size")
     particle_size = 0
 
+m_p = p*1000/(2.688*10^25)
 g = 9.81
 re = 1*10**-6
 c_d = 24/re
 p_a = 0.9093
-a = ((1/4)*(math.pi)*(d_p)**2)
+a = ((1/4)*(math.pi)*(d)**2)
 
 # Drag is in the x-direction. Everything else is in the y-direction.
 a = (-(m_p * g) - ((1/2)(c_d * (p_s - p_a) * a * u_r * u__r)) + ((1/6) * (math.pi) * (p_s - p_a) * g * (d**3)) + (voltage) / (dist_pp))) / (m_p)
