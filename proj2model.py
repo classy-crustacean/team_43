3dd4b32725236bcba0a3c2096ce1e403bbe8ebc2
Joe Rosloniec <62963029+classy-crustacean@users.noreply.github.com>
2022-04-02 11:23:42 -0400

    Update proj2model.py

--- proj2model.py
+++ proj2model.py
@@ -5,23 +5,33 @@ dist_pp = int(input("Enter the distance between the parallel plates (m): "))
 u_r = int(input("Enter the rate of airflow (m/s): ")) # This is just in the positive x-direction
 voltage = int(input("Enter the voltage (V): "))
 
-# Particle size 1 represents PM2.5, and 2 represents PM10
-particle_size = int(input("Choose particle size:\n1: PM2.5\n2: PM10\n"))
+
 
 area_pp = length_pp * width _pp
 
 # Set values and constants
 
-m_p = ?
+# Particle size 1 represents PM2.5, and 2 represents PM10
+particle_size = 0
+while (particle_size == 0)
+  particle_size = int(input("Choose particle size:\n1: PM2.5\n2: PM10\n"))
+  if (particle_size == 1):
+    m_p = ?
+    d = 0.0000000025
+    p = 0.0000000092
+  elif (particle_size == 2):
+    m_p = ?
+    d = 0.00000001
+    p = 0.0000000116
+  else:
+    print("invalid particle size")
+    particle_size = 0
+
 g = 9.81
 re = 1*10**-6
 c_d = 24/re
 p_a = 0.9093
-d = ?
 a = ((1/4)*(math.pi)*(d_p)**2)
-p = 0.0000000092 
-# The density of PM2.5 particles is 0.0000000092 kg/m^3
-# The density of PM10 particles 0.0000000116 kg/m^3
 
 # Drag is in the x-direction. Everything else is in the y-direction.
 a = (-(m_p * g) - ((1/2)(c_d * (p_s - p_a) * a * u_r * u__r)) + ((1/6) * (math.pi) * (p_s - p_a) * g * (d**3)) + (voltage) / (dist_pp))) / (m_p)
